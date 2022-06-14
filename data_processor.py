a56b12ee722a6adb116604def9a86812c5750619
fze0012 <fze0012@stu.xjtu.edu.cn>
2022-06-14 15:40:42 +0800

    first commit

---
+++ data_processor.py
@@ -0,0 +1,41 @@
+import torchvision
+import torch
+
+
+def data_loader(args):
+    kwopt = {'num_workers': 8, 'pin_memory': True}
+    trn_transforms = torchvision.transforms.Compose([
+        torchvision.transforms.RandomCrop(args.image_size),
+        torchvision.transforms.RandomHorizontalFlip(),
+        torchvision.transforms.ToTensor(),
+    ])
+
+    # Transformers for set5 and set14
+    # In quantitative comparsions, we resize all images into 256*256.
+    # In visual comparisons, we crop all image into a*b where a and b are multiple of 32.
+    test_set5 = torchvision.transforms.Compose([
+        torchvision.transforms.Resize((256, 256)),
+        torchvision.transforms.ToTensor(),
+    ])
+    test_set14 = torchvision.transforms.Compose([
+        torchvision.transforms.Resize((256, 256)),
+        torchvision.transforms.ToTensor(),
+    ])
+
+    # Transformers for BSDS
+    val_bsds = torchvision.transforms.Compose([
+        torchvision.transforms.CenterCrop((480, 320)),
+        torchvision.transforms.ToTensor(),
+    ])
+    
+    trn_dataset = torchvision.datasets.ImageFolder('./BSDS500/train', transform=trn_transforms)
+    val_bsds = torchvision.datasets.ImageFolder('./BSDS500/val', transform=val_bsds)
+    test_set5 = torchvision.datasets.ImageFolder('./BSDS500/set5', transform=test_set5)
+    test_set14 = torchvision.datasets.ImageFolder('./BSDS500/set14', transform=test_set14)
+
+    trn_loader = torch.utils.data.DataLoader(trn_dataset, batch_size=args.batch_size, shuffle=True, **kwopt, drop_last=False)
+    val_loader_bsds = torch.utils.data.DataLoader(val_bsds, batch_size=1, shuffle=True, **kwopt, drop_last=False)
+    test_loader_set5 = torch.utils.data.DataLoader(test_set5, batch_size=1, shuffle=True, **kwopt, drop_last=False)
+    test_loader_set14 = torch.utils.data.DataLoader(test_set14, batch_size=1, shuffle=True, **kwopt, drop_last=False)
+
+    return trn_loader, val_loader_bsds, test_loader_set5, test_loader_set14
---
+++ trainer.py
@@ -0,0 +1,65 @@
+from utils import *
+import time
+import cv2
+from torchvision.transforms import ToPILImage
+import torchvision
+
+
+def train(train_loader, model, criterion, optimizer, epoch):
+    print('Epoch: %d' % (epoch + 1))
+    model.train()
+    sum_loss = 0
+    for inputs, _ in train_loader:
+        inputs = rgb_to_ycbcr(inputs.cuda())[:, 0, :, :].unsqueeze(1) / 255.
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs[0], inputs) + criterion(outputs[1], inputs)
+        loss.backward()
+        optimizer.step()
+        sum_loss += loss.item()
+    return sum_loss
+
+
+def valid_bsds(valid_loader, model, criterion):
+    sum_psnr = 0
+    sum_ssim = 0
+    _ssim = SSIM().cuda()
+    model.eval()
+    with torch.no_grad():
+        for iters, (inputs, _) in enumerate(valid_loader):
+            inputs = rgb_to_ycbcr(inputs.cuda())[:, 0, :, :].unsqueeze(1) / 255.
+            outputs = model(inputs)
+            mse = F.mse_loss(outputs[0], inputs)
+            psnr = 10 * log10(1 / mse.item())
+            sum_psnr += psnr
+            sum_ssim += ssim(outputs[0], inputs)
+
+    return sum_psnr / len(valid_loader), sum_ssim / len(valid_loader)
+
+
+def valid_set(valid_loader, model, criterion):
+    sum_psnr = 0
+    sum_ssim = 0
+    _ssim = SSIM().cuda()
+    model.eval()
+    time_sum = 0
+    with torch.no_grad():
+        for iters, (inputs, _) in enumerate(valid_loader):
+            inputs = rgb_to_ycbcr(inputs.cuda())[:, 0, :, :].unsqueeze(1) / 255.
+
+            # torch.cuda.synchronize()
+            # start = time.time()
+
+            # torchvision.utils.save_image(inputs.data, '%s.tif' % ('ground'), padding=0)
+            outputs = model(inputs)
+
+            # torch.cuda.synchronize()
+            # end = time.time()
+
+            # time_sum = time_sum + (end - start)
+            mse = F.mse_loss(outputs[0], inputs)
+            psnr = 10 * log10(1 / mse.item())
+            sum_psnr += psnr
+            sum_ssim += ssim(outputs[0], inputs)
+            # torchvision.utils.save_image(outputs[0].data, '%.2f_%.4f.tif' % (psnr, sum_ssim), padding=0)
+    return sum_psnr / len(valid_loader), sum_ssim / len(valid_loader)
